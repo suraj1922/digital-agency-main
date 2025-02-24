0cb49f0b47bbca91220bb1ad3f224ce9a2557c8a
SurajGupta <surajgupta19224@gmail.com>
2025-02-24 17:42:27 +0530

    ..

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -33,7 +33,7 @@ const ResponsiveMenu = ({ showMenu }) => {
       </div>
       <div className="footer">
         <h1>
-          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Dilshad</a>{" "}
+          Made with ❤ by <a href="https://github.com/suraj1922/">Suraj</a>{" "}
         </h1>
       </div>
     </div>
